d54ab6cb4d2342623eb54ecd10929a8697205fe2
qanhta2710 <quanganhta2710@gmail.com>
2025-04-05 10:32:47 +0700

    Change Comments

--- pp_chia_doi/pp_chia_doi_cho_epsilon.c
+++ pp_chia_doi/pp_chia_doi_cho_epsilon.c
@@ -5,12 +5,10 @@ double func(double x) {
     return 2 * pow(x, 5) + 12 * pow(x, 4) - 5 * pow(x, 3) + 7 * x - 15; // Nhap phuong trinh f(x)
 }
 
-// CAM DONG VAO DAY
-
 void printSolution(double (*func)(double), double a, double b, double epsilon) {
     int n = log((b - a) / epsilon) + 1;
     printf("%3s %15s %15s %15s %15s\n", "n", "a", "b", "c", "f(c)");
-    double c = (a + b) / 2; // initial value
+    double c = (a + b) / 2; 
     double z = func(c);
     printf("%3d %15.10lf %15.10lf %15.10lf %15.10e\n", 0, a, b, c, z);
     for (int i = 1; i < n; i++) {
@@ -30,9 +28,11 @@ void printSolution(double (*func)(double), double a, double b, double epsilon) {
     }
 }
 int main() {
-    double a = 0; // Nhap khoang cach li a
-    double b = 1; // Nhap khoang cach li b
-    double epsilon = 0.5 * pow(10, -7); // Nhap sai so
+    // Nhap khoang cach li (a, b)
+    double a = 0; 
+    double b = 1; 
+    // Nhap sai so
+    double epsilon = 0.5 * pow(10, -7); 
     printSolution(func, a, b, epsilon);
     return 0;
 }
